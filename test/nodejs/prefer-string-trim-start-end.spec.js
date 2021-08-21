373386b932708ee6c6504d41888f58b78692ad2d
Richard Huang <flyerhzm@gmail.com>
2021-08-21 11:27:23 +0800

    add nodejs/prefer-string-trim-start-end snippet

---
+++ test/nodejs/prefer-string-trim-start-end.spec.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const mock = require('mock-fs')
+const Synvert = require("synvert-core")
+require('../../lib/nodejs/prefer-string-trim-start-end')
+
+describe("Prefer String trimStart and trimEnd", () => {
+  const input = `
+    const foo1 = bar.trimLeft();
+    const foo2 = bar.trimRight();
+  `
+  const output = `
+    const foo1 = bar.trimStart();
+    const foo2 = bar.trimEnd();
+  `
+  beforeEach(() => {
+    mock({ 'code.js': input })
+  })
+  afterEach(() => {
+    mock.restore()
+  })
+
+  test("convert", () => {
+    const rewriter = Synvert.Rewriter.fetch('nodejs', 'preferStringTrimStartEnd')
+    rewriter.process()
+    expect(fs.readFileSync('code.js', 'utf-8')).toEqual(output)
+  })
+})
