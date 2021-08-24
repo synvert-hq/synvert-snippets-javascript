a50252542a3bb60a0904f446abc6f5ebaf5fb22c
Richard Huang <flyerhzm@gmail.com>
2021-08-24 15:25:00 +0800

    prefer bind operator

---
+++ test/javascript/prefer-bind-operator.spec.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const mock = require('mock-fs')
+const Synvert = require("synvert-core")
+require('../../lib/javascript/prefer-bind-operator')
+
+describe("Prefer bind Operator", () => {
+  const input = `
+    let x = this.foo.bind(this);
+  `
+  const output = `
+    let x = ::this.foo;
+  `
+  beforeEach(() => {
+    mock({ 'code.js': input })
+  })
+  afterEach(() => {
+    mock.restore()
+  })
+
+  test("convert", () => {
+    const rewriter = Synvert.Rewriter.fetch('javascript', 'preferBindOperator')
+    rewriter.process()
+    expect(fs.readFileSync('code.js', 'utf-8')).toEqual(output)
+  })
+})
