a7ac7ba5f9dabdb583e7362a6e25ce4ea410e5eb
Richard Huang <flyerhzm@gmail.com>
2021-08-25 16:51:26 +0800

    add javascript/prefer-object-has-own snippet

---
+++ test/javascript/prefer-object-has-own.spec.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const mock = require('mock-fs')
+const Synvert = require("synvert-core")
+require('../../lib/javascript/prefer-object-has-own')
+
+describe("Prefer Object hasOwn", () => {
+  const input = `
+    Object.prototype.hasOwnProperty.call({ prop: 42 }, 'prop')
+  `
+  const output = `
+    Object.hasOwn({ prop: 42 }, 'prop')
+  `
+  beforeEach(() => {
+    mock({ 'code.js': input })
+  })
+  afterEach(() => {
+    mock.restore()
+  })
+
+  test("convert", () => {
+    const rewriter = Synvert.Rewriter.fetch('javascript', 'preferObjectHasOwn')
+    rewriter.process()
+    expect(fs.readFileSync('code.js', 'utf-8')).toEqual(output)
+  })
+})
