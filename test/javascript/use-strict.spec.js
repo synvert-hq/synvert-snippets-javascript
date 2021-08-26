cccf15714c4485e95f71e7b725a58d96e9192a45
Richard Huang <flyerhzm@gmail.com>
2021-08-26 16:03:55 +0800

    add javascript/use-strict snippet

---
+++ test/javascript/use-strict.spec.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const mock = require('mock-fs')
+const Synvert = require("synvert-core")
+require('../../lib/javascript/use-strict')
+
+describe("Use strict", () => {
+  describe("exists", () => {
+    const input = `'use strict'\nfoobar`
+    const output = `'use strict'\nfoobar`
+    beforeEach(() => {
+      mock({ 'code.js': input })
+    })
+    afterEach(() => {
+      mock.restore()
+    })
+
+    test("convert", () => {
+      const rewriter = Synvert.Rewriter.fetch('javascript', 'useStrict')
+      rewriter.process()
+      expect(fs.readFileSync('code.js', 'utf-8')).toEqual(output)
+    })
+  })
+
+  describe("does not exist", () => {
+    const input = `foobar`
+    const output = `'use strict'\nfoobar`
+    beforeEach(() => {
+      mock({ 'code.js': input })
+    })
+    afterEach(() => {
+      mock.restore()
+    })
+
+    test("convert", () => {
+      const rewriter = Synvert.Rewriter.fetch('javascript', 'useStrict')
+      rewriter.process()
+      expect(fs.readFileSync('code.js', 'utf-8')).toEqual(output)
+    })
+  })
+})
