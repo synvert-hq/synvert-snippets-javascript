38adfe165a1fe70a252fc435a0e01fc62d274947
Richard Huang <flyerhzm@gmail.com>
2021-08-28 15:11:40 +0800

    add javascript/trailing-comma snippet

---
+++ test/javascript/trailing-comma.spec.js
@@ -0,0 +1,61 @@
+const fs = require('fs')
+const mock = require('mock-fs')
+const Synvert = require("synvert-core")
+require('../../lib/javascript/trailing-comma')
+
+describe("Trailing Comma", () => {
+  const input = `
+    const object = {
+      hello: 'hello',
+      allo: 'allo',
+      hola: 'hola'
+    };
+
+    const object2 = {hello: 'hello', allo: 'allo', hola: 'hola'};
+
+    const array = [
+      'hello',
+      'allo',
+      'hola'
+    ];
+
+    const array2 = ['hello', 'allo', 'hola'];
+
+    test({
+      test: 'test'
+    });
+  `
+  const output = `
+    const object = {
+      hello: 'hello',
+      allo: 'allo',
+      hola: 'hola',
+    };
+
+    const object2 = {hello: 'hello', allo: 'allo', hola: 'hola'};
+
+    const array = [
+      'hello',
+      'allo',
+      'hola',
+    ];
+
+    const array2 = ['hello', 'allo', 'hola'];
+
+    test({
+      test: 'test',
+    });
+  `
+  beforeEach(() => {
+    mock({ 'code.js': input })
+  })
+  afterEach(() => {
+    mock.restore()
+  })
+
+  test("convert", () => {
+    const rewriter = Synvert.Rewriter.fetch('javascript', 'trailingComma')
+    rewriter.process()
+    expect(fs.readFileSync('code.js', 'utf-8')).toEqual(output)
+  })
+})
