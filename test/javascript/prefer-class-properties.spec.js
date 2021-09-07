c12dd250889c60dc4182237e85d9a567ed5c4f00
Richard Huang <flyerhzm@gmail.com>
2021-09-07 19:35:42 +0800

    add javascript/prefer-class-properties snippet

---
+++ test/javascript/prefer-class-properties.spec.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+const mock = require('mock-fs')
+const Synvert = require("synvert-core")
+require('../../lib/javascript/prefer-class-properties')
+
+describe("Prefer class properties", () => {
+  const input = `
+    class Button extends Component {
+      constructor(props) {
+        super(props);
+        this.state = { clicked: false };
+        this.handleClick = this.handleClick.bind(this);
+      }
+
+      handleClick() {
+        this.setState({ clicked: true });
+      }
+
+      render() {
+        return <button onClick={this.handleClick}>Click Me!</button>;
+      }
+    }
+  `
+  const output = `
+    class Button extends Component {
+      constructor(props) {
+        super(props);
+        this.state = { clicked: false };
+        this.handleClick = this.handleClick.bind(this);
+      }
+
+      handleClick = () => {
+        this.setState({ clicked: true });
+      }
+
+      render() {
+        return <button onClick={this.handleClick}>Click Me!</button>;
+      }
+    }
+  `
+  beforeEach(() => {
+    mock({ 'code.jsx': input })
+  })
+  afterEach(() => {
+    mock.restore()
+  })
+
+  test("convert", () => {
+    const rewriter = Synvert.Rewriter.fetch('javascript', 'preferClassProperties')
+    rewriter.process()
+    expect(fs.readFileSync('code.jsx', 'utf-8')).toEqual(output)
+  })
+})
