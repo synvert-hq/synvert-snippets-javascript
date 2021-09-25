0c93cfbc27a0d5136a6e2bd835c4ca68d0f32053
Richard Huang <flyerhzm@gmail.com>
2021-09-25 13:49:16 +0800

    add javascript/preferImportExport snippet

---
+++ test/javascript/prefer-import-export.spec.js
@@ -0,0 +1,26 @@
+require("../../lib/javascript/prefer-import-export");
+const { assertConvert } = require("../utils");
+
+describe("Prefer import export", () => {
+  input = `
+    const fs = require('fs'),
+          path = require('path')
+    const { Node } = require('acorn')
+
+    module.exports = Rewriter
+    module.exports = { Rewriter, Configuration }
+  `
+  output = `
+    import fs from 'fs'
+    import path from 'path'
+    import { Node } from 'acorn'
+
+    export default Rewriter
+    export { Rewriter, Configuration }
+  `
+  assertConvert({
+    input,
+    output,
+    snippet: "javascript/preferImportExport",
+  });
+});
