bb7acbfd634cea02abe470c71f89d848da42135e
Richard Huang <flyerhzm@gmail.com>
2022-01-02 17:59:00 +0800

    first version of react/transfer-class-components-to-functions

---
+++ test/react/transfer-class-components-to-functions.spec.js
@@ -0,0 +1,85 @@
+require("../../lib/react/transfer-class-components-to-functions");
+const { assertConvert } = require("../utils");
+
+describe("react/transfer-class-components-to-functions", () => {
+  describe('simple compoment', () => {
+    const input = `
+      class MyComponent extends React.Component {
+        render() {
+          return <p>It works!</p>;
+        }
+      }
+    `;
+
+    const output = `
+      const MyComponent = (props) => {
+        return <p>It works!</p>;
+      }
+    `;
+
+    assertConvert({
+      input,
+      output,
+      path: "code.jsx",
+      snippet: "react/transfer-class-components-to-functions",
+    });
+  });
+
+  describe('this.props', () => {
+    const input = `
+      class MyComponent extends React.Component {
+        render() {
+          return <p>Hi, {this.props.name}</p>;
+        }
+      }
+    `;
+
+    const output = `
+      const MyComponent = (props) => {
+        return <p>Hi, {props.name}</p>;
+      }
+    `;
+
+    assertConvert({
+      input,
+      output,
+      path: "code.jsx",
+      snippet: "react/transfer-class-components-to-functions",
+    });
+  });
+
+  describe('this.state', () => {
+    const input = `
+      class MyComponent extends React.Component {
+        state = { count: 0 };
+
+        render() {
+          const reset = () => this.setState({ count: 0 });
+          const inc = () => this.setState({ count: this.state.count + 1 });
+          return <p onClick={inc}>
+            Total: {this.state.count}
+          </p>;
+        }
+      }
+    `;
+
+    const output = `
+      const MyComponent = (props) => {
+        const [count, setCount] = useState(0);
+
+        const reset = () => setCount(0);
+        const inc = () => setCount(count + 1);
+        return <p onClick={inc}>
+          Total: {count}
+        </p>;
+      }
+    `;
+
+    assertConvert({
+      input,
+      output,
+      path: "code.jsx",
+      snippet: "react/transfer-class-components-to-functions",
+    });
+  });
+});
