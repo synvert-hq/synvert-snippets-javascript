87bb2c7c081c2e2b955b14f2f2b85a5757af969b
Richard Huang <flyerhzm@gmail.com>
2022-01-21 19:22:35 +0800

    add jquery/deprecate-ajax-success-error-and-complete snippet

---
+++ test/jquery/deprecate-ajax-success-error-and-complete.spec.js
@@ -0,0 +1,28 @@
+require("../../lib/jquery/deprecate-ajax-success-error-and-complete");
+const { assertConvert } = require("../utils");
+
+describe("jquery/deprecate-ajax-success-error-and-complete", () => {
+  assertConvert({
+    input: `
+      $.ajax({
+        url: 'URL',
+        type: 'POST',
+        data: yourData,
+        datatype: 'json',
+        success: function (data) { successFunction(data); },
+        error: function (jqXHR, textStatus, errorThrown) { errorFunction(); }
+      });
+    `,
+    output: `
+      $.ajax({
+        url: 'URL',
+        type: 'POST',
+        data: yourData,
+        datatype: 'json',
+      })
+      .done(function (data) { successFunction(data); })
+      .fail(function (jqXHR, textStatus, errorThrown) { errorFunction(); });
+    `,
+    snippet: "jquery/deprecate-ajax-success-error-and-complete",
+  });
+});
