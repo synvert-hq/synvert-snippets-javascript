2255eaf16b1ca828490be0bf807b33f335cc9741
Richard Huang <flyerhzm@gmail.com>
2022-01-31 13:30:25 +0800

    add react/upgrade-to-18 snippet

---
+++ test/react/upgrade-to-18.spec.js
@@ -0,0 +1,23 @@
+const snippet = 'react/upgrade-to-18'
+require(`../../lib/${snippet}`);
+const { assertConvert } = require("../utils");
+
+describe(snippet, () => {
+  const input = `
+    const container = document.getElementById('root');
+    ReactDOM.render(<App />, container);
+  `;
+
+  const output = `
+    const container = document.getElementById('root');
+    const root = ReactDOM.createRoot(container);
+    root.render(<App />);
+  `;
+
+  assertConvert({
+    input,
+    output,
+    path: "code.jsx",
+    snippet,
+  });
+});
