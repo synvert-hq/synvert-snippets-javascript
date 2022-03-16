a2719306b65c4d42d40c3bc4f02cbbb88334ce33
Richard Huang <flyerhzm@gmail.com>
2022-03-16 16:50:41 +0800

    add synvert/deprecate-fix-indent-to-source snippet

---
+++ test/synvert/deprecate-fix-indent-to-source.spec.js
@@ -0,0 +1,27 @@
+const snippet = "synvert/deprecate-fix-indent-to-source"
+require(`../../lib/${snippet}`);
+const { assertConvert } = require("../utils");
+
+describe(snippet, () => {
+  const input = `
+    new Synvert.Rewriter("synvert", "deprecate-fix-indent-to-source", () => {
+      withinFiles(Synvert.ALL_FILES, function () {
+        this.currentNode.fixIndentToSource()
+      });
+    });
+  `
+
+  const output = `
+    new Synvert.Rewriter("synvert", "deprecate-fix-indent-to-source", () => {
+      withinFiles(Synvert.ALL_FILES, function () {
+        this.currentNode.toSource({ fixIndent: true })
+      });
+    });
+  `
+
+  assertConvert({
+    input,
+    output,
+    snippet,
+  });
+});
