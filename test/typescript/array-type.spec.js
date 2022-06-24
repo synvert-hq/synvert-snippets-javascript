5d969cac99f37ac9ee9a8d1c38a62005e26f42b2
Richard Huang <flyerhzm@gmail.com>
2022-06-24 09:51:49 +0800

    add typescript/array-type snippet

---
+++ test/typescript/array-type.spec.js
@@ -0,0 +1,24 @@
+const snippet = "typescript/array-type";
+require(`../../lib/${snippet}`);
+const { assertConvert } = require("../utils");
+
+describe(snippet, () => {
+  const input = `
+    const x: Array<string> = ['a', 'b'];
+    const y: ReadonlyArray<string> = ['a', 'b'];
+    const z: Array<string | number> = ['a', 'b'];
+  `;
+
+  const output = `
+    const x: string[] = ['a', 'b'];
+    const y: readonly string[] = ['a', 'b'];
+    const z: (string | number)[] = ['a', 'b'];
+  `;
+
+  assertConvert({
+    path: "code.ts",
+    input,
+    output,
+    snippet,
+  });
+});
