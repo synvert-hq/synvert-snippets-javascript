4ac345c2b54d07cd77bf3986c86088187d7745d9
Richard Huang <flyerhzm@gmail.com>
2022-09-26 12:17:50 +0800

    add javascript/prefer-string-slice snippet

---
+++ test/javascript/prefer-string-slice.spec.js
@@ -0,0 +1,21 @@
+const snippet = "javascript/prefer-string-slice";
+require(`../../lib/${snippet}`);
+const { assertConvert } = require("../utils");
+
+describe(snippet, () => {
+  const input = `
+    foo.substr(start, length);
+    foo.substring(indexStart, indexEnd);
+  `;
+
+  const output = `
+    foo.slice(start, start + length);
+    foo.slice(indexStart, indexEnd);
+  `;
+
+  assertConvert({
+    input,
+    output,
+    snippet,
+  });
+});
