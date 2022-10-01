f425bf197dc4e7c468bf10b0c3a7b615ca5cc00f
Richard Huang <flyerhzm@gmail.com>
2022-10-01 14:20:15 +0800

    add javascript/prefer-negative-index snippet

---
+++ test/javascript/prefer-negative-index.spec.js
@@ -0,0 +1,23 @@
+const snippet = "javascript/prefer-negative-index";
+require(`../../lib/${snippet}`);
+const { assertConvert } = require("../utils");
+
+describe(snippet, () => {
+  const input = `
+    foo.slice(foo.length - 2, foo.length - 1);
+    foo.splice(foo.length - 1, 1);
+    foo.at(foo.length - 1);
+  `;
+
+  const output = `
+    foo.slice(-2, -1);
+    foo.splice(-1, 1);
+    foo.at(-1);
+  `;
+
+  assertConvert({
+    input,
+    output,
+    snippet,
+  });
+});
