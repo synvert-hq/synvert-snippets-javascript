c102e70d49c21d43cc7ceb801445ef5fac48f77d
Richard Huang <flyerhzm@gmail.com>
2022-10-18 15:52:58 +0800

    add helpers/remove-imports

---
+++ test/helpers/remove-imports.spec.js
@@ -0,0 +1,172 @@
+const { assertHelper } = require("../utils");
+
+describe("helpers/remove-imports", () => {
+  describe("all valid", () => {
+    assertHelper({
+      input: `
+        import x from "package";
+        import { a, b } from "./utils";
+        import y from "package";
+        const c = a() + b + x() + y();
+      `,
+      output: `
+        import x from "package";
+        import { a, b } from "./utils";
+        import y from "package";
+        const c = a() + b + x() + y();
+      `,
+      helper: 'helpers/remove-imports',
+      options: { importNames: [] },
+    });
+  });
+
+  describe("first specifier is unused", () => {
+    assertHelper({
+      input: `
+        import x from "package";
+        import { a, b } from "./utils";
+        import y from "package";
+        const c = b(x, y);
+      `,
+      output: `
+        import x from "package";
+        import { b } from "./utils";
+        import y from "package";
+        const c = b(x, y);
+      `,
+      helper: 'helpers/remove-imports',
+      options: { importNames: ['a'] },
+    });
+  });
+
+  describe("last specifier is unused", () => {
+    assertHelper({
+      input: `
+        import { a, b } from "./utils";
+        import y from "package";
+        /**
+         * this is a jsdoc!
+         */
+        const c = a(y);
+      `,
+      output: `
+        import { a } from "./utils";
+        import y from "package";
+        /**
+         * this is a jsdoc!
+         */
+        const c = a(y);
+      `,
+      helper: 'helpers/remove-imports',
+      options: { importNames: ['b'] },
+    });
+  });
+
+  describe("all specifiers are unused", () => {
+    assertHelper({
+      input: `
+        import { a, b } from "./utils";
+        import y from "package";
+        const c = 4;
+        console.log(y);
+      `,
+      output: `
+        import y from "package";
+        const c = 4;
+        console.log(y);
+      `,
+      helper: 'helpers/remove-imports',
+      options: { importNames: ['a', 'b'] },
+    });
+  });
+
+  describe("all specifiers are unused 2", () => {
+    assertHelper({
+      input: `
+        import c, { a, b } from "./utils";
+        console.log(c);
+      `,
+      output: `
+        import c from "./utils";
+        console.log(c);
+      `,
+      helper: 'helpers/remove-imports',
+      options: { importNames: ['a', 'b'] },
+    });
+  });
+
+  describe("default specifier is unused", () => {
+    assertHelper({
+      input: `
+        import { a, b } from "./utils";
+        import y from "package";
+        const c = a() + b;
+      `,
+      output: `
+        import { a, b } from "./utils";
+        const c = a() + b;
+      `,
+      helper: 'helpers/remove-imports',
+      options: { importNames: ['y'] },
+    });
+  });
+
+  describe("default specifier is unused 2", () => {
+    assertHelper({
+      input: `
+        import y, { a, b } from "./utils";
+        const c = a() + b;
+      `,
+      output: `
+        import { a, b } from "./utils";
+        const c = a() + b;
+      `,
+      helper: 'helpers/remove-imports',
+      options: { importNames: ['y'] },
+    });
+  });
+
+  describe("default specifier is unused 3", () => {
+    assertHelper({
+      input: `
+        import y, * as p from "package";
+        p.test();
+      `,
+      output: `
+        import * as p from "package";
+        p.test();
+      `,
+      helper: 'helpers/remove-imports',
+      options: { importNames: ['y'] },
+    });
+  });
+
+  describe("namespace specifier is unused", () => {
+    assertHelper({
+      input: `
+        import * as p from "package";
+        console.log("p");
+      `,
+      output: `
+        console.log("p");
+      `,
+      helper: 'helpers/remove-imports',
+      options: { importNames: ['p'] },
+    });
+  });
+
+  describe("namespace specifier is unused 2", () => {
+    assertHelper({
+      input: `
+        import y, * as p from "package";
+        console.log(y);
+      `,
+      output: `
+        import y from "package";
+        console.log(y);
+      `,
+      helper: 'helpers/remove-imports',
+      options: { importNames: ['p'] },
+    });
+  });
+});
--- test/utils.js
+++ test/utils.js
@@ -1,14 +1,15 @@
 const fs = require("fs");
+const path = require("path");
 const mock = require("mock-fs");
 const Synvert = require("synvert-core");
 
 const assertConvert = (options) => {
-  const path = options["path"] || "code.js";
+  const snippetPath = options["path"] || "code.js";
   const input = options["input"];
   const output = options["output"];
 
   beforeEach(() => {
-    mock({ [path]: input });
+    mock({ [snippetPath]: input });
   });
 
   afterEach(() => {
@@ -19,8 +20,39 @@ const assertConvert = (options) => {
     const [group, name] = options["snippet"].split("/");
     const rewriter = Synvert.Rewriter.fetch(group, name);
     rewriter.process();
-    expect(fs.readFileSync(path, "utf-8")).toEqual(output);
+    expect(fs.readFileSync(snippetPath, "utf-8")).toEqual(output);
   });
 };
 
-module.exports = { assertConvert };
+const assertHelper = (options) => {
+  const helperPath = options["path"] || "code.js";
+  const input = options["input"];
+  const output = options["output"];
+
+  beforeEach(() => {
+    process.env.SYNVERT_SNIPPETS_HOME = path.join(__dirname, "..")
+    const helperLibraryPath = path.join(process.env.SYNVERT_SNIPPETS_HOME, 'lib', options.helper + '.js');
+    const helperContent = fs.readFileSync(helperLibraryPath, 'utf-8');
+    mock({
+      [helperLibraryPath]: helperContent,
+      [helperPath]: input
+    });
+  });
+
+  afterEach(() => {
+    mock.restore();
+  });
+
+  test("convert", () => {
+    const rewriter = new Synvert.Rewriter("group", "name", () => {
+      configure({ parser: "typescript" });
+      withinFiles("*.{js,jsx}", () => {
+        callHelper(options.helper, options.options);
+      });
+    });
+    rewriter.process();
+    expect(fs.readFileSync(helperPath, "utf-8")).toEqual(output);
+  });
+};
+
+module.exports = { assertConvert, assertHelper };
