d828254d106110a30df9903693fe85dea1b0eaac
Richard Huang <flyerhzm@gmail.com>
2023-02-06 16:10:21 +0800

    migrate all jquery snippets to use typescript adapter

--- test/jquery/deprecate-error.spec.js
+++ test/jquery/deprecate-error.spec.js
@@ -3,8 +3,8 @@ const { assertConvert } = require("../utils");
 
 describe(snippet, () => {
   assertConvert({
-    input: `$(this).error(function () { console.log('error') });`,
-    output: `$(this).on('error', function () { console.log('error') });`,
+    input: `$(this).error(function () { console.log("error") });`,
+    output: `$(this).on("error", function () { console.log("error") });`,
     snippet,
   });
 });
--- test/jquery/deprecate-load-unload.spec.js
+++ test/jquery/deprecate-load-unload.spec.js
@@ -4,12 +4,12 @@ const { assertConvert } = require("../utils");
 describe(snippet, () => {
   assertConvert({
     input: `
-      $(this).load(function () { console.log('load') });
-      $(this).unload(function () { console.log('unload') });
+      $(this).load(function () { console.log("load") });
+      $(this).unload(function () { console.log("unload") });
     `,
     output: `
-      $(this).on('load', function () { console.log('load') });
-      $(this).on('unload', function () { console.log('unload') });
+      $(this).on("load", function () { console.log("load") });
+      $(this).on("unload", function () { console.log("unload") });
     `,
     snippet,
   });
