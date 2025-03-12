d3e566292233e07fadfc858f9edcf5c1060114d0
Richard Huang <flyerhzm@gmail.com>
2025-03-12 13:51:55 +0800

    update snippet react/prevent-default

--- test/react/prevent-default.spec.js
+++ test/react/prevent-default.spec.js
@@ -4,7 +4,7 @@ const { assertConvert } = require("../utils");
 describe(snippet, () => {
   describe('add a function to prevent default', () => {
     const input = `
-      const Post = ({ newPost, editPost, doDelete }) => {
+      const Post = ({ newPost, editPost, doDelete, onPrint, doPop }) => {
         const onEditPost = (event) => {
           event.preventDefault();
 
@@ -15,16 +15,24 @@ describe(snippet, () => {
           doDelete();
         }
 
+        const printIcon = (
+          <a href="#" onClick={() => onPrint()}>
+            <img src="/svg/print.svg" alt="Print" className="marginlft10" />
+          </a>
+        );
+
         return (
           <a href="#" onClick={newPost}>New</a>
           <a href="#" onClick={onEditPost}>Edit</a>
           <a href="#" onClick={deletePost}>Delete</a>
+          {printIcon}
+          <a href="#" onClick={() => { doPop(false) }}>Pop</a>
         )
       }
       export default Post;
     `;
     const output = `
-      const Post = ({ newPost, editPost, doDelete }) => {
+      const Post = ({ newPost, editPost, doDelete, onPrint, doPop }) => {
         const onEditPost = (event) => {
           event.preventDefault();
 
@@ -37,16 +45,36 @@ describe(snippet, () => {
           doDelete();
         }
 
+        const printIcon = (
+          <a href="#" onClick={handlePrint}>
+            <img src="/svg/print.svg" alt="Print" className="marginlft10" />
+          </a>
+        );
+
+        const handlePrint = (event) => {
+          event.preventDefault();
+
+          onPrint();
+        }
+
         const onNewPost = (event) => {
           event.preventDefault();
 
           newPost();
         }
 
+        const onDoPop = (event) => {
+          event.preventDefault();
+
+          doPop(false);
+        }
+
         return (
           <a href="#" onClick={onNewPost}>New</a>
           <a href="#" onClick={onEditPost}>Edit</a>
           <a href="#" onClick={deletePost}>Delete</a>
+          {printIcon}
+          <a href="#" onClick={onDoPop}>Pop</a>
         )
       }
       export default Post;
